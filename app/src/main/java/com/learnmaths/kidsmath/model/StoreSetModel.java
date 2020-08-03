2c7a2699c46ba030d9b282a87055f738973ec1fc
roony4 <roony4>
2020-08-03 16:57:51 +0530

    Initial commit

---
+++ app/src/main/java/com/learnmaths/kidsmath/model/StoreSetModel.java
@@ -0,0 +1,13 @@
+package com.learnmaths.kidsmath.model;
+
+public class StoreSetModel {
+
+    public String title, category;
+    public int id;
+
+    public StoreSetModel(int id, String title, String category) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+    }
+}
